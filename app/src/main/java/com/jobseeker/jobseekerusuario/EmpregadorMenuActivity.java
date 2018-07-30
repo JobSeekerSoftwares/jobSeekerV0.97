53ec32b8e66f2de765d408c53ec08ac37776c272
AndreLz <andre.luizpvieira@gmail.com>
2018-07-30 18:11:50 -0400

    Adicionado menus e informações melhores

--- app/src/main/java/com/jobseeker/jobseekerusuario/EmpregadorMenuActivity.java
+++ app/src/main/java/com/jobseeker/jobseekerusuario/EmpregadorMenuActivity.java
@@ -10,11 +10,16 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.app.sample.fchat.ChatActivity;
+import com.app.sample.fchat.data.SettingsAPI;
 import com.app.sample.fchat.data.Tools;
+import com.app.sample.fchat.widget.CircleTransform;
 import com.jobseeker.jobseekerusuario.Model.Empregador;
 import com.jobseeker.jobseekerusuario.Model.Trabalhador;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,13 +31,20 @@ import retrofit2.Response;
 public class EmpregadorMenuActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private Trabalhador usuario;
+    public ImageView image;
+    SettingsAPI set;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empregador_menu);
+        set = new SettingsAPI(this);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         prepareActionBar(toolbar);
         usuario = getIntent().getParcelableExtra("usuario");
+
+        ImageView img =  (ImageView) findViewById(R.id.image55);
+
+        Picasso.with(getBaseContext()).load(set.readSetting("mydp")).resize(150, 150).transform(new CircleTransform()).into(img);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Tools.systemBarLolipop(this);
         }
@@ -74,17 +86,26 @@ public class EmpregadorMenuActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void changeToTrabalhador(View view) {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        intent.putExtra("usuario", usuario);
+        startActivity(intent);
+    }
+
     public void changeMeusEmpregos(View view) {
         ApiClient.getJobClient().getMeusEmpregos(usuario.getIdGmail()).enqueue(new Callback<List<Empregador>>() {
             public void onResponse(Call<List<Empregador>> call, Response<List<Empregador>> response ){
                 if (response.isSuccessful() ) {
 
                     List<Empregador> jobs = response.body();
-                    Intent intent = new Intent(getBaseContext(), ListJobToChangeActivity.class);
-                    intent.putExtra("usuario", usuario);
-                    intent.putParcelableArrayListExtra("meusJobs",(ArrayList)jobs);
-                    startActivity(intent);
-
+                    if(jobs.size() == 0){
+                        Toast.makeText(getApplicationContext() , "Você não possui ofertas de empregos registrados", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Intent intent = new Intent(getBaseContext(), ListJobToChangeActivity.class);
+                        intent.putExtra("usuario", usuario);
+                        intent.putParcelableArrayListExtra("meusJobs", (ArrayList) jobs);
+                        startActivity(intent);
+                    }
                 } else {
                     System.out.println(response.errorBody());
                     Toast.makeText(getApplicationContext() , "Erro encontrado", Toast.LENGTH_SHORT).show();
@@ -96,6 +117,12 @@ public class EmpregadorMenuActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void goChat(View view) {
+        Intent intent = new Intent(getBaseContext(), ChatActivity.class);
+        intent.putExtra("usuario", usuario);
+        startActivity(intent);
     }
 
 }
